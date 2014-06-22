272ff36ac127cbc785031e2509fac793b44f2988
Werner Strydom <werners@bloudraak.com>
2014-06-22 14:10:33 -0700

    Added a cachifier class library with an single class

---
+++ src/Cachifier.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) 2012-2014, Werner Strydom
+// All rights reserved.
+
+namespace Cachifier
+{
+    /// <summary>
+    /// Represents a task to Cachify a website
+    /// </summary>
+    public class CachifyTask
+    {
+    }
+}
---
+++ src/Cachifier.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("Cachifier Core Library")]
+[assembly: AssemblyDescription("The core functionality to classify code")]
+[assembly: AssemblyCompany("Werner Strydom")]
+[assembly: AssemblyProduct("Cachifier")]
+[assembly: AssemblyCopyright("Copyright © Werner Strydom 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("0f90128e-94cd-4705-aeb5-b523d59a7632")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
