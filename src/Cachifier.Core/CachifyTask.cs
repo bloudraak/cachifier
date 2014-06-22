89bc323013998c853c59bb6a37f3135a552fb2e2
Werner Strydom <werners@bloudraak.com>
2014-06-22 14:20:46 -0700

    Added Resharper Settings

--- src/Cachifier.Core/CachifyTask.cs
+++ src/Cachifier.Core/CachifyTask.cs
@@ -24,18 +24,12 @@
 
 #endregion
 
-using System.Reflection;
-using System.Resources;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Cachifier Core Library")]
-[assembly: AssemblyDescription("The core functionality to classify code")]
-[assembly: AssemblyCompany("Werner Strydom")]
-[assembly: AssemblyProduct("Cachifier")]
-[assembly: AssemblyCopyright("Copyright © Werner Strydom 2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("0f90128e-94cd-4705-aeb5-b523d59a7632")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: NeutralResourcesLanguage("en-US")]
+namespace Cachifier
+{
+    /// <summary>
+    ///     Represents a task to Cachify a website
+    /// </summary>
+    public class CachifyTask
+    {
+    }
+}
