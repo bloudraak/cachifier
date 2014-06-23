e986b223bbb7962bd2d9347bf5d85bdebbf5e672
Werner Strydom <werners@bloudraak.com>
2014-06-23 10:01:27 -0700

    Some minor refactorings

--- src/Cachifier.Build.Tasks/CachifierTask.cs
+++ src/Cachifier.Build.Tasks/CachifierTask.cs
@@ -29,7 +29,6 @@ namespace Cachifier.Build.Tasks
     using System;
     using System.Collections.Generic;
     using System.ComponentModel;
-    using System.Diagnostics;
     using System.IO;
     using System.Linq;
     using System.Text.RegularExpressions;
@@ -43,20 +42,27 @@ namespace Cachifier.Build.Tasks
     [PublicAPI]
     public class CachifierTask : Task
     {
+        private IEncoder _encoder;
+        private string _extensionPattern;
+        private Regex _extensionRegex;
+        private IHashifier _hashifier;
+
         /// <summary>
-        /// Gets the content files
+        ///     Gets the content files
         /// </summary>
         [Required]
+        [PublicAPI]
         public ITaskItem[] Content
         {
-            get; 
+            get;
             set;
         }
 
         /// <summary>
-        /// Gets the embedded resources in the project
+        ///     Gets the embedded resources in the project
         /// </summary>
         [Required]
+        [PublicAPI]
         public ITaskItem[] EmbeddedResources
         {
             get;
@@ -64,6 +70,7 @@ namespace Cachifier.Build.Tasks
         }
 
         [Required]
+        [PublicAPI]
         public ITaskItem[] StaticExtensions
         {
             get;
@@ -71,26 +78,29 @@ namespace Cachifier.Build.Tasks
         }
 
         /// <summary>
-        /// Gets the output folders
+        ///     Gets the output folders
         /// </summary>
         [Required]
+        [PublicAPI]
         public string OutputPath
         {
-            get; 
+            get;
             set;
         }
 
         /// <summary>
-        /// Gets the items the output items
+        ///     Gets the items the output items
         /// </summary>
         [Output]
+        [PublicAPI]
         public ITaskItem[] OutputItems
         {
-            get; 
+            get;
             private set;
         }
 
         [Required]
+        [PublicAPI]
         public string ProjectDirectory
         {
             get;
@@ -98,12 +108,12 @@ namespace Cachifier.Build.Tasks
         }
 
         [DefaultValue(false)]
+        [PublicAPI]
         public bool Override
         {
             get;
             set;
         }
-
 
         /// <summary>
         ///     When overridden in a derived class, executes the task.
@@ -115,71 +125,60 @@ namespace Cachifier.Build.Tasks
         {
             try
             {
-                if (Content.Length == 0)
+                if (this.Content.Length == 0)
                 {
-                    string message = string.Format("There are no content items to process");
-                    var args = new BuildMessageEventArgs(message, string.Empty, "CachifierNoContent", MessageImportance.Normal);
-                    BuildEngine.LogMessageEvent(args);
+                    var message = string.Format("There are no content items to process");
+                    var args = new BuildMessageEventArgs(message,
+                        string.Empty,
+                        "CachifierNoContent",
+                        MessageImportance.Normal);
+                    this.BuildEngine.LogMessageEvent(args);
                 }
 
-                var extensionPattern = string.Join("|", StaticExtensions.Select(s => Regex.Escape(s.ItemSpec)));
-                var extensionRegex = new Regex(extensionPattern, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+                this._extensionPattern = string.Join("|", this.StaticExtensions.Select(s => Regex.Escape(s.ItemSpec)));
+                this._extensionRegex = new Regex(this._extensionPattern,
+                    RegexOptions.IgnoreCase | RegexOptions.Singleline);
+                this._hashifier = new Hashifier();
+                this._encoder = new Encoder();
                 var outputItems = new List<ITaskItem>();
-                IHashifier hashifier = new Hashifier();
-                IEncoder encoder = new Encoder();
-                var filesToDelete = new List<string>();
-
-                var outputDirectoryName = Path.Combine(ProjectDirectory, OutputPath);
-                if (Directory.Exists(outputDirectoryName))
-                {
-                    filesToDelete.AddRange(Directory.GetFiles(outputDirectoryName, "*", SearchOption.AllDirectories));
-                }
-
-                var files = new List<string>();
+                var outputDirectoryName = Path.Combine(this.ProjectDirectory, this.OutputPath);
+                var filesToDelete = GetFilesToDelete(outputDirectoryName);
+                var files = new HashSet<string>();
                 var mapping = new Dictionary<string, string>();
 
-                foreach (var contentFile in EmbeddedResources)
+                foreach (var contentFile in this.EmbeddedResources)
                 {
-                    this.ProcessTaskItem(contentFile, extensionRegex, hashifier, encoder, filesToDelete, mapping, outputDirectoryName, files, outputItems);
+                    this.ProcessTaskItem(contentFile, filesToDelete, mapping, outputDirectoryName, files, outputItems);
                 }
 
-                foreach (var contentFile in Content)
+                foreach (var contentFile in this.Content)
                 {
-                    this.ProcessTaskItem(contentFile, extensionRegex, hashifier, encoder, filesToDelete, mapping, outputDirectoryName, files, outputItems);
-
-                    // TODO: Replace relative paths
+                    this.ProcessTaskItem(contentFile, filesToDelete, mapping, outputDirectoryName, files, outputItems);
                 }
 
                 if (Directory.Exists(outputDirectoryName))
                 {
                     foreach (var file in filesToDelete)
                     {
-                        Log(MessageImportance.Normal, "Deleting file \"{0}\" because it is no longer referenced by the project.", file);
+                        this.Log(MessageImportance.Normal,
+                            "Deleting file \"{0}\" because it is no longer referenced by the project.",
+                            file);
                         File.Delete(file);
                     }
                     this.DeleteEmptyDirectories(outputDirectoryName);
                 }
 
+                this.OutputItems = outputItems.ToArray();
 
-                OutputItems = outputItems.ToArray();
-               
-            
                 foreach (var file in files)
                 {
-                    var relativePath = Processor.GetRelativePath(file, ProjectDirectory);
+                    var relativePath = Processor.GetRelativePath(file, this.ProjectDirectory);
 
-                    bool skip = true;
-                    switch (Path.GetExtension(file))
+                    if (IsBinary(file))
                     {
-                        case ".css":
-                        case ".js":
-                            skip = false;
-                            break;
-                    }
-
-                    if (skip)
-                    {
-                        Log(MessageImportance.Normal, "Skipping file \"{0}\" because it is not a static resource.", relativePath);
+                        this.Log(MessageImportance.Normal,
+                            "Skipping file \"{0}\" because it is not a static resource.",
+                            relativePath);
                         continue;
                     }
 
@@ -194,11 +193,15 @@ namespace Cachifier.Build.Tasks
                     if (!originalText.Equals(text))
                     {
                         File.WriteAllText(file, text);
-                        Log(MessageImportance.Normal, "Updating file \"{0}\" since references to other static resources has changed.", relativePath);
+                        this.Log(MessageImportance.Normal,
+                            "Updating file \"{0}\" since references to other static resources has changed.",
+                            relativePath);
                     }
                     else
                     {
-                        Log(MessageImportance.Normal, "Skipping file \"{0}\" since references to other static resources has not changed.", relativePath);
+                        this.Log(MessageImportance.Normal,
+                            "Skipping file \"{0}\" since references to other static resources has not changed.",
+                            relativePath);
                     }
                 }
 
@@ -206,37 +209,72 @@ namespace Cachifier.Build.Tasks
             }
             catch (Exception e)
             {
-                Log(MessageImportance.High, "Exception: {0}", e);
+                this.Log(MessageImportance.High, "Exception: {0}", e);
                 return false;
             }
         }
 
-        private void ProcessTaskItem(ITaskItem contentFile, Regex extensionRegex, IHashifier hashifier, IEncoder encoder,
-                                     List<string> filesToDelete, Dictionary<string, string> mapping, string outputDirectoryName, List<string> files,
-                                     List<ITaskItem> outputItems)
+        private static HashSet<string> GetFilesToDelete(string outputDirectoryName)
+        {
+            HashSet<string> filesToDelete;
+            if (Directory.Exists(outputDirectoryName))
+            {
+                var items = Directory.EnumerateFiles(outputDirectoryName, "*", SearchOption.AllDirectories);
+                filesToDelete = new HashSet<string>(items);
+            }
+            else
+            {
+                filesToDelete = new HashSet<string>();
+            }
+            return filesToDelete;
+        }
+
+        private static bool IsBinary(string file)
+        {
+            bool skip = true;
+            switch (Path.GetExtension(file))
+            {
+                case ".css":
+                case ".js":
+                    skip = false;
+                    break;
+            }
+            return skip;
+        }
+
+        private void ProcessTaskItem(
+            ITaskItem contentFile,
+            ICollection<string> filesToDelete,
+            IDictionary<string, string> mapping,
+            string outputDirectoryName,
+            ICollection<string> files,
+            ICollection<ITaskItem> outputItems)
         {
             var path = contentFile.ItemSpec;
             var extension = Path.GetExtension(path);
             if (extension == null)
             {
-                //this.Log("Skipping '{0}' since it has no extension", path);
+                this.Log(MessageImportance.Normal,
+                    "Skipping '{0}' because it doesn't represent a static resource.",
+                    path);
                 return;
             }
 
-            if (!extensionRegex.IsMatch(extension))
+            if (!this._extensionRegex.IsMatch(extension))
             {
-                this.Log(MessageImportance.Low, "Skipping '{0}'. It doesn't represent a static file.", path);
+                this.Log(MessageImportance.Normal,
+                    "Skipping '{0}' because it doesn't represent a static resource.",
+                    path);
                 return;
             }
 
             var fullPath = contentFile.GetMetadata("FullPath");
-            var hashValue = hashifier.Hashify(fullPath);
-            var encodedHashValue = encoder.Encode(hashValue);
+            var hashValue = this._hashifier.Hashify(fullPath);
+            var encodedHashValue = this._encoder.Encode(hashValue);
             var relativePath = Processor.GetRelativePath(fullPath, this.ProjectDirectory);
             var hashedFileName = Path.GetFileNameWithoutExtension(fullPath) + "," + encodedHashValue
                                  + Path.GetExtension(fullPath);
-            var outputPath = Path.Combine(this.ProjectDirectory, this.OutputPath, relativePath);
-            outputPath = Path.Combine(Path.GetDirectoryName(outputPath), hashedFileName);
+            var outputPath = Path.Combine(outputDirectoryName, hashedFileName);
             var relativeOutputPath = Processor.GetRelativePath(outputPath, this.ProjectDirectory);
 
             var directoryName = Path.GetDirectoryName(outputPath);
@@ -307,6 +345,5 @@ namespace Cachifier.Build.Tasks
             var eventArgs = new BuildMessageEventArgs(message, string.Empty, "CachifierProcessingContent", importance);
             this.BuildEngine.LogMessageEvent(eventArgs);
         }
-
     }
 }
